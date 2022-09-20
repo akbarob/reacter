5b4bc0089fcb50acbdb525e57469140ad0c66c71
akbarob <akbarbadmus07@gmail.com>
2022-09-20 22:11:11 +0100

    delay

--- src/Components/section2.js
+++ src/Components/section2.js
@@ -35,7 +35,7 @@ function D (){
       }
 
       const boxVariant = {
-        visible: { opacity: 1, x:0,  transition:{duration:0.8} },
+        visible: { opacity: 1, x:0,  transition:{duration:0.5} },
         hidden: { opacity: 0,  x:'-100%' },
     }
  const card = Info.map(item =>{
